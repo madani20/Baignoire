0c679ad0742ec8201163fee4e0019e4a504657c3
madani <khmadani@free.fr>
2019-02-07 17:17:42 +0100

    Ajout d'une classe Ciseaux.java

---
+++ Ciseaux.java
@@ -0,0 +1,21 @@
+package baignoire;
+
+public class Ciseaux {
+	
+	private String nom;
+	
+	public Ciseaux(String nom){
+		this.nom=nom;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	
+
+}
